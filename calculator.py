84f1a29be2cf6c233c5c157d20280fef0d89751b
Mateo McAllister <mateomcallisterschool@gmail.com>
2025-04-14 16:14:22 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,31 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
+
+# https://github.com/mateomcalli/lab10-MM-JL
+# Partner 1: Mateo McAllister | Partner 2: Jamie Laird
+
+import math
+
 def add(a, b): 
-    pass
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    if a != 0:
+        return b/a
+    else: raise ZeroDivisionError
+
+def log(a,b):
+    return math.log(b,a)
+
+def exp(a,b):
+    return a**b
+
 
 
