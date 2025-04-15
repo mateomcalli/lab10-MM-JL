ef78a9c3f360b4c14d6601b35e74f6c091935e8c
jrosecs <119766669+jrosecs@users.noreply.github.com>
2025-04-14 20:48:52 -0400

    Potential div Fix

--- calculator.py
+++ calculator.py
@@ -32,12 +32,9 @@ def mul(a, b):
     return a * b
 
 def div(a, b):
-    try:
         if a == 0:
             raise ZeroDivisionError
         return b / a
-    except ZeroDivisionError as e:
-        print(e)
 
 def exp(a, b):
     return a ** b
